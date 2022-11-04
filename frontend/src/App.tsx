97128c85835fdf30154dbdff819763358410f8c1
ikaika <willtolivehaver@gmail.com>
2022-11-04 14:11:45 -0500

    home page header finished

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import HomePageHeader from "./components/HomePageHeader";
+import { Container, Grid } from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Container>
+			<Grid
+				container
+				direction='column'
+				justifyContent='center'
+				alignItems='center'
+			>
+				<Grid item sx={{ width: "100%" }}>
+					<HomePageHeader location='Baton Rouge' />
+				</Grid>
+			</Grid>
+		</Container>
+	);
 }
 
 export default App;
