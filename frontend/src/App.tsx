089a7cbb3c39f272899e78cdf38f27bbe337acdd
Chris Chuang <67641657+cchuan3@users.noreply.github.com>
2022-11-13 20:47:14 -0600

    Listing Description
    Component for listing description box with uploader's profile pic/username, date, and description text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ListingDescription from "./components/ListingDescription";
+
+const testDescription = {description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", publishDate: "01/01/2022"};
+const seller = {username: "NotARobot", profileImagePath:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.jHuJyHBQHuvP9BaqeSEdYAHaFg%26pid%3DApi&f=1&ipt=81fef1be5a99d7a59d8923a01c891943774666ff4c4519667b31f76f65442703&ipo=images"};
+// const seller = {username: "NotARobot", profileImagePath:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.6jnkAUhi_xp42OdSeqJeaAAAAA%26pid%3DApi&f=1&ipt=f3a3498b4132a5d88f8d142019e8361aae786962c74a7f8c10780031607de97c&ipo=images"};
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<ListingDescription seller={seller} listingDesc={testDescription}/>
 	);
 }
 
