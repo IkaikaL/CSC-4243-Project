561d87fc26d48a6dc94853759b1e75da49efa38f
ikaika <willtolivehaver@gmail.com>
2022-11-14 20:16:50 -0600

    changed to black text in footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -12,22 +12,22 @@ const Footer = () => {
 			>
 				<Grid item>
 					<a href='https://www.craigslist.org/about/help/'>
-						<Button sx={{ width: "200px", color: "white" }}>Help</Button>
+						<Button sx={{ width: "200px", color: "black" }}>Help</Button>
 					</a>
 				</Grid>
 				<Grid item>
 					<a href='https://www.craigslist.org/about/help/faq'>
-						<Button sx={{ width: "200px", color: "white" }}>FAQ</Button>
+						<Button sx={{ width: "200px", color: "black" }}>FAQ</Button>
 					</a>
 				</Grid>
 				<Grid item>
 					<a href='https://www.craigslist.org/about/terms.of.use/en'>
-						<Button sx={{ width: "200px", color: "white" }}>Legal</Button>
+						<Button sx={{ width: "200px", color: "black" }}>Legal</Button>
 					</a>
 				</Grid>
 				<Grid item>
 					<a href='https://www.craigslist.org/about/privacy.policy'>
-						<Button sx={{ width: "200px", color: "white" }}>
+						<Button sx={{ width: "200px", color: "black" }}>
 							Privacy Policy
 						</Button>
 					</a>
