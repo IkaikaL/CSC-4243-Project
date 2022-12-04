935e899fc778f1c4092745d9fb29a7c329bc3bda
SpadesXion <35231383+SpadesXion@users.noreply.github.com>
2022-12-04 14:49:32 -0600

    Update SortButton.tsx

--- frontend/src/components/SortButton/SortButton.tsx
+++ frontend/src/components/SortButton/SortButton.tsx
@@ -1,20 +1,18 @@
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import { Container } from "@mui/material/"
+import { Card, Grid } from "@mui/material/"
 
 export default function SortButton() {
     return (
-
-                <Container
-                    sx={{width: '100%',}}
-                    style = {{background: 'gray', border: "1px solid white"}}>
+            <Grid 
+            style = {{backgroundColor: 'lightgray',}}
+            sx = {{  width: '190px', height: '35px', paddingLeft: '5px'}}>
                     <p className = 'SortBox'> Sort by: 
                         <select className='SelectionBoxStyle' id = "select2">
                         <option value = "oldest">Oldest</option>
                         <option value = "newest">Newest</option>
                         <option value = "most rated">Most Rated</option>
-
-                        </select> <ArrowDownwardIcon /> </p>
-                </Container>
+                        </select> <ArrowDownwardIcon />
+                    </p>
+            </Grid>
     );
 }
-
