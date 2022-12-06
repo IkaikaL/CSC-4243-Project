2709d987425906e3818786759af947d41ff6ff45
SpadesXion <35231383+SpadesXion@users.noreply.github.com>
2022-12-05 18:29:43 -0600

    Update Reviews.tsx

--- frontend/src/components/Reviews/Reviews.tsx
+++ frontend/src/components/Reviews/Reviews.tsx
@@ -1,14 +1,58 @@
-import React from 'react'
 import LikeCount from './LikeCount/LikeCount'
-
 import StarIcon from '@mui/icons-material/Star';
 import Paper from '@mui/material/Paper';
 import { Grid, Container } from "@mui/material";
 
+// function formatReview(name: string, StarRating: number, date: string, verified: boolean, review: string) {
+//     return (
+//         if (verified == true) {
+        
+//         var confirmVerified: string;
+//         confirmVerified = 'Verified purchase';
+//         var stars = StarRating;
+
+//         <Grid item style = {classes.reviewgrid}>
+//                     <Paper style = {classes.paper}>
+//                         <Grid container direction = 'row'>
+//                             <Grid sx = {{width: '150px'}}>
+//                                 if (stars == 5) {
+//                                         <p style = {classes.starUpColor}> <StarIcon /><StarIcon /><StarIcon /><StarIcon /><StarIcon /> </p>
+//                                     }
+//                                 else if (stars == 4) {
+//                                         <p style = {classes.starUpColor}> <StarIcon /><StarIcon /><StarIcon /><StarIcon /></p>
+//                                     }
+//                                 else if (stars == 3) {
+//                                         <p style = {classes.starUpColor}><StarIcon /><StarIcon /><StarIcon /> </p>
+//                                     }
+//                                 else if (stars == 2) {
+//                                         <p style = {classes.starUpColor}><StarIcon /><StarIcon /> </p>
+//                                     }
+//                                 else if (stars == 1) {
+//                                         <p style = {classes.starUpColor}><StarIcon /> </p>
+//                                     }
+
+//                                 <p style = {classes.starUpColor}> <StarIcon /><StarIcon /><StarIcon /> </p>
+//                                 <p> By {name} <br />Submitted <br /> {date} <br /> {confirmVerified} </p>
+//                                 <p> <LikeCount /></p>
+//                             </Grid>
+//                             <Grid sx = {{ width: '400px' }}>
+//                                 <p> {review} </p>    
+//                             </Grid>
+//                         </Grid>
+//                     </Paper>
+//                 </Grid>
+        
+//         }
+//         else {
+//             return
+//         }
+//     )
+// }
+
 function UserInfo (props: any) {
     return (
         <div>
-            By {props.name} <br />Submitted {props.date} <br /> {props.verified}
+            By {props.name} <br />Submitted <br /> {props.date} <br /> {props.verified}
         </div>
     );
 }
@@ -27,96 +71,78 @@ const classes = {
         marginBottom: '10px',
         backgroundColor: 'white',
         padding: '10px',
+    },
 
-    }
+    reviewgrid: {
+        width: "600px",
+        padding: '10px',
+    },
+    
+    starUpColor: {
+        color: 'yellow',
+        
+    },
+
+    starDownColor: {
+        color: 'lightgray',
+    },
 }
-
-// type Props = {
-//     setInfo: Array<string>
-//     name: string;
-//     date: string;
-//     verified: string;
-//     review: string;
-// }
-
-// class reviewinfo {
-//     star: number;
-//     name:string;
-//     date:string;
-//     verified:boolean;
-//     review:string;
-
-//     constructor(star:number, name:string, date:string,verified:boolean, review:string){
-//         this.star = star;
-//         this.name = name;
-//         this.date = date;
-//         this.verified = verified;
-//         this.review = review;
-//     }
-// }
 
 
 const Reviews = (props: any) => {
-    const {setInfo, name, date, verified, review } = props;
     return (
-        <Container sx={{ width: "600px" }} >
-            <Grid
-                container
-                spacing={3}
-                direction='column'
-                alignItems='left'
-                justifyContent='center'
-                sx={{ width: "100%", height: "100%" }}
-            >
-                <Grid item sx = {{ width: "1000px"}}>
+            <Grid>
+                <Grid item style = {classes.reviewgrid}>
                     <Paper style ={classes.paper}>
-                        <Grid
-                        >
+                        <Grid container direction = 'row'>
+                            <Grid sx = {{width: '150px'}}>
+                                <span style = {classes.starUpColor}> <StarIcon /><StarIcon /><StarIcon /><StarIcon /><StarIcon /> </span>  
+                                <p className = 'buyerid'> <UserInfo name = "boatluver1855" date = "Nov. 18, 2019" verified = "Verified purchase" /></p>
+                                <p className =  'dateid'><LikeCount /></p>
+                            </Grid>
+                            <Grid sx = {{ width: '400px' }}>
+                                <UserReview review = "Merchandise was well kept. Very knowledgable on engines. Would buy from again." />
+                            </Grid>
 
-                        <div className = 'topright_reviewbox'> 
-                            <p className = 'stars'> <StarIcon /><StarIcon /><StarIcon /><StarIcon /><StarIcon /> </p>  
-                            <p className = 'buyerid'> <UserInfo name = "boatluver1855" date = "Nov. 18, 2019" verified = "Verified purchase" /></p>
-                            <p className =  'dateid'><LikeCount /></p>
-                        </div>
-                        </Grid>
-                        <Grid>
-                        <div className = 'ReviewContent_Container'>
-                            <UserReview review = "Merchandise was well kept. Very knowledgable on engines. Would buy from again." />
-                        </div>
                         </Grid>
                     </Paper>
                 </Grid>
-                <Grid item sx = {{ width: "1000px", padding: '10px', spacing: 4}}>
-                <Paper style = {classes.paper}>
-                <div className = 'Review_Divider'>
-                    <div className = 'Review_Container'>
-                        <div className='topright_reviewbox'>
-                            <p className = 'stars'> <StarIcon /><StarIcon /><StarIcon /> </p>
-                            <p className = 'buyerid'> <UserInfo name = "mmregan511" date = "Jan. 6, 2021" verified = "Verified purchase"/> </p>
-                            <p className = 'dateid'> <LikeCount /></p>
-                        </div>
-                        <div className = 'ReviewContent_Container'>
-                            <UserReview review = "Arrived at the place to take a look at his stuff. Didn't tell me in advance that he was going to be late, so I ended up waiting for two hours before he got to our meeting location." />
-                        </div>
-                    </div>
-                </div>
-                </Paper>
+
+                <Grid item style = {classes.reviewgrid}>
+                    <Paper style = {classes.paper}>
+                        <Grid container direction = 'row'>
+                            <Grid sx = {{width: '150px'}}>
+                                <p>
+                                    <span style = {classes.starUpColor}><StarIcon /> <StarIcon /> <StarIcon /> </span>
+                                    {/* <span style = {classes.starDownColor}><StarIcon /> <StarIcon /> </span> */}
+                                </p>
+                                <p> <UserInfo name = "mmregan511" date = "Jan. 6, 2021" verified = "Verified purchase"/> </p>
+                                <p> <LikeCount /></p>
+                            </Grid>
+                            <Grid sx = {{ width: '400px' }}>
+                                <UserReview review = "Arrived at the place to take a look at his stuff. Didn't tell me in advance that he was going to be late, so I ended up waiting for two hours before he got to our meeting location." />
+                            </Grid>
+                        </Grid>
+                    </Paper>
                 </Grid>
-                <div className = 'Review_Divider'>
-                    <div className = 'Review_Container'>
-                        <div className='topright_reviewbox'>
-                            <p className = 'stars'> <StarIcon /><StarIcon /><StarIcon /><StarIcon /> </p>
-                            <p className = 'buyerid'> <UserInfo name = "jaxjil_xXx_4154" date = "Jun. 19, 2021" verified = "Verified purchase"/> </p>
-                            <p className = 'dateid'> <LikeCount /></p>
-                        </div>
-                        <div className = 'ReviewContent_Container'>
+
+                <Grid item style = {classes.reviewgrid}>
+                    <Paper style = {classes.paper}>
+                        <Grid container direction = 'row'>
+                            <Grid sx = {{width: '150px', marginTop: '15px' }}>
+                                <span style = {classes.starUpColor}><StarIcon /> <StarIcon /> <StarIcon /> <StarIcon /> </span>
+                                <span style = {classes.starDownColor}><StarIcon /></span>
+                                <p> <UserInfo name = "jaxjil_xXx_4154" date = "Jun. 19, 2021" verified = "Verified purchase"/> </p>
+                                <p> <LikeCount /></p>
+                            </Grid>
+                            <Grid sx = {{ width: '400px' }}>
                             <UserReview review = "Got what I wanted. Case was damaged on the side, which wasn't told on the listing." />
-                        </div>
-                    </div>
-                </div>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Grid>
             </Grid>
-        </Container>
-)
+    )
 }
 
 export default Reviews;
